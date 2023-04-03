7b8b9f5636dd77a4339c97680420e9adc2fd0ef6
JaneDoe <janedoe@notanemail.com>
2023-04-03 18:50:21 -0500

    Design and fields

---
+++ restaurant/Menu.java
@@ -0,0 +1,17 @@
+package restaurant;
+
+import java.util.Date;
+import java.util.ArrayList;
+
+import restaurant.MenuItem;
+
+class Menu {
+	
+	private ArrayList<MenuItem> items;
+	private MenuItem mostRecent;
+	private Date lastUpdate;
+
+	public void Menu () {
+	
+	}
+}
---
+++ restaurant/MenuItem.java
@@ -0,0 +1,27 @@
+package restaurant;
+
+import java.util.Date;
+import java.util.ArrayList;
+
+
+class MenuItem {
+
+	private String name;
+	private String category;
+	private String description;
+	private String proteinType;
+
+	private ArrayList<String> allergens;
+
+	private boolean isVegan;
+	private boolean isAvailable;
+
+	private int spiceLevel;
+	private double price;
+
+	private Date dateAdded;
+
+	public void MenuItem () {
+	
+	}
+}
